e95b30fe7c4ecbc3f2f77d2e82a158b51d6b15d7
Esteban Tejeda <etejedaw@hotmail.com>
2022-12-23 12:04:14 -0300

    feat: add index.ts to import htmlExtractor lib

--- src/helpers/htmlExtractor/index.ts
+++ src/helpers/htmlExtractor/index.ts
@@ -1,5 +1,6 @@
 import Getter from "./Getter";
 import Puppeteer from "./puppeteer/Puppeteer";
 import RequestPromise from "./requestPromise/RequestPromise";
+import HtmlExtractor from "./htmlExtractor.intereface";
 
-export { Getter, Puppeteer, RequestPromise };
+export { Getter, Puppeteer, RequestPromise, HtmlExtractor };
